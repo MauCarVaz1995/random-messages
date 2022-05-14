efb82bcbc24306ce50a141b066ffef8092d6ee5e
Mauricio Carrillo <mcarrillov183@alumno.uaemex.mx>
2022-05-14 12:36:14 -0500

    prueba

--- bin/global.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-//Elemento que se ejecuta dentro del sistema bash
-
-//Crear variable para requerir alo que acabo de crear dentro de mi paquete
-let random = require("../src/index");
-
-//Con esto se ejecuta en la terminal
-random.randomMsg();
